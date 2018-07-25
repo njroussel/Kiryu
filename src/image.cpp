505b9008a5219a50a3c5352c0315990deda2e9d0
Nicolas Roussel <n.roussel13@gmail.com>
2018-07-25 09:54:36 +0200

    Add missing consts in Image::writePng, fix memory leak where image data was not freed after being written

--- src/image.cpp
+++ src/image.cpp
@@ -9,7 +9,7 @@
 #include <kiryu/image.h>
 
 uint8_t* Image::loadImage(const std::string &filename, int &width, int &height,
-        int &channels, int desiredChannels)
+        int &channels, const int desiredChannels)
 {
     return stbi_load(filename.c_str(), &width, &height, &channels,
             desiredChannels);
@@ -18,16 +18,21 @@ uint8_t* Image::loadImage(const std::string &filename, int &width, int &height,
 bool Image::writePng(const std::string &filename, const int width,
         const int height, const int channels, const float *const data)
 {
-    uint8_t *uint8Data = new uint8_t[width * height * channels];
+    uint8_t *const uint8Data = new uint8_t[width * height * channels];
 
     for (int i = 0; i < width; i++) {
         for (int j = 0; j < height; j++) {
             for (int k = 0; k < channels; k++) {
-                int index = j * width * channels + i * channels + k;
+                const int index = j * width * channels + i * channels + k;
                 uint8Data[index] = 255 * data[index];
             }
         }
     }
-    return stbi_write_png(filename.c_str(), width, height, channels,
+
+    const bool noError = stbi_write_png(filename.c_str(), width, height, channels,
             uint8Data, width * channels);
+
+    delete[] uint8Data;
+
+    return noError;
 }
