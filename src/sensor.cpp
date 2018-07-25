ab0ccee3fe13da73afdbf86fc7deb980ba527c07
Nicolas Roussel <n.roussel13@gmail.com>
2018-07-25 09:50:23 +0200

    Make everything (possible) in const in Sensor methods

--- src/sensor.cpp
+++ src/sensor.cpp
@@ -3,32 +3,33 @@
 
 #include <kiryu/sensor.h>
 
-Sensor::Sensor(Vector3f position, Vector3f target, Vector3f up, Float fov,
-        uint16_t width, uint16_t height) :
+Sensor::Sensor(const Vector3f position, const Vector3f target, const Vector3f up,
+        const Float fov, const uint16_t width, const uint16_t height) :
     m_position(position), m_target(target.normalized()), m_up(up.normalized()),
     m_fov(fov), m_width(width), m_height(height) { }
 
-Ray3f Sensor::generateRay(uint16_t xPixel, uint16_t yPixel, Float xSample,
-        Float ySample)
+Ray3f Sensor::generateRay(const uint16_t xPixel, const uint16_t yPixel,
+        const Float xSample, const Float ySample)
 {
-    Float sensorEpsilon = 1e-4;
+    const Float sensorEpsilon = 1e-4;
 
-    Float worldPixelSize = sensorEpsilon * std::atan(m_fov / 2.0f)  * 2.0f / m_width;
+    const Float worldPixelSize =
+        sensorEpsilon * std::atan(m_fov / 2.0f)  * 2.0f / m_width;
 
-    Vector3f nearPlaneHorizon = m_target.cross(m_up);
+    const Vector3f nearPlaneHorizon = m_target.cross(m_up);
 
-    Vector3f worldTopLeftNearPlane =
+    const Vector3f worldTopLeftNearPlane =
         m_position +
         m_target * sensorEpsilon +
         nearPlaneHorizon * worldPixelSize * -m_width / 2.0f +
         m_up * worldPixelSize * m_height / 2.0f;
 
-    Vector3f rayOrigin =
+    const Vector3f rayOrigin =
         worldTopLeftNearPlane +
         (xPixel + xSample) * worldPixelSize * nearPlaneHorizon +
         (yPixel + ySample) * worldPixelSize * -m_up; 
 
-    Vector3f rayDirection = (rayOrigin - m_position).normalized();
+    const Vector3f rayDirection = (rayOrigin - m_position).normalized();
 
     return Ray3f(rayOrigin, rayDirection);
 }
